02600acce95cce5a43a85cb56ea3164889c01745
Agus Cahyono <cahyo.mamen@gmail.com>
2020-03-22 20:54:37 +0700

    Update AppDelegate.swift

--- Maps Direction/AppDelegate.swift
+++ Maps Direction/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
 
-	var googleAPIKey = "AIzaSyBPg6a5c6LnUn89J61Zv7rKC-UdsbfKqGU"
+	var googleAPIKey = "YourApiKey"
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
